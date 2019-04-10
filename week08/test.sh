63ab3da31adcf8ef95e195c55758a3a7260decb1
z5169797 <z5169797@cse.unsw.edu.au>
2019-04-10 16:06:45 +1000

    weighted graph

---
+++ week08/test.sh
@@ -0,0 +1,26 @@
+echo "$ ./travel Berlin Chicago"
+./travel Berlin Chicago 
+echo "$ ./travel Sydney Chicago"
+./travel Sydney Chicago
+echo "$ ./travel Sydney London"
+./travel Sydney London
+echo "$ ./travel London Sydney"
+./travel London Sydney
+echo "$ ./travel Sydney 'Buenos Aires'"
+./travel Sydney 'Buenos Aires'
+echo "$ ./travel Sydney London 6000"
+./travel Sydney London 6000
+echo "$ ./travel Sydney 'Buenos Aires' 5000"
+./travel Sydney 'Buenos Aires' 5000
+echo "$ ./travel Sydney 'Buenos Aires' 7000"
+./travel Sydney 'Buenos Aires' 7000
+echo "$ ./travel Sydney 'Buenos Aires' 8000"
+./travel Sydney 'Buenos Aires' 8000
+echo "$ ./travel Sydney 'Buenos Aires' 11000"
+./travel Sydney 'Buenos Aires' 11000
+echo "$ ./travel Sydney 'Buenos Aires' 12000"
+./travel Sydney 'Buenos Aires' 12000
+echo "$ ./travel Bombay Chicago 5000"
+./travel Bombay Chicago 5000
+echo "$ ./travel Sydney Sydney"
+./travel Sydney Sydney
